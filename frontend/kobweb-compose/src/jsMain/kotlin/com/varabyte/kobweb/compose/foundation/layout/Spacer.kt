25e7ba2a9525221a258065fc65f43c2e9e1e6771
Yongshun Shreck Ye <ShreckYe@gmail.com>
2024-11-11 23:09:40 +0800

    Add a `Modifier` parameter to `Spacer`

--- frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/foundation/layout/Spacer.kt
+++ frontend/kobweb-compose/src/jsMain/kotlin/com/varabyte/kobweb/compose/foundation/layout/Spacer.kt
@@ -1,12 +1,14 @@
 package com.varabyte.kobweb.compose.foundation.layout
 
 import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.toAttrs
 import org.jetbrains.compose.web.dom.Div
 
 /**
  * An element which grows to consume all remaining space in a [Row] or [Column].
  */
 @Composable
-fun Spacer() {
-    Div(attrs = { classes("kobweb-spacer") })
+fun Spacer(modifier: Modifier = Modifier) {
+    Div(attrs = modifier.toAttrs { classes("kobweb-spacer") })
 }
